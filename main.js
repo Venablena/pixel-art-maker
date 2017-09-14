cb44ab061922940b5fa50bd16814706281ccb68f
Lena Venable <venablena@gmail.com>
2017-09-14 16:28:16 -0700

    changed eventHandler structure for color picking functions

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 // document.addEventListener("DOMContentLoaded", function (){
-  console.log("Sanity check");
+console.log("Sanity check");
 
-  let squares = document.getElementsByClassName('square')
-    for (let i = 0; i < squares.length; i++) {
-      let pixel = squares[i]
+let color = "";
+document.querySelectorAll('.circle').forEach(function(pen){
+  pen.addEventListener('click', function(){
+    color = this.id
+  })
+});
 
-      pixel.addEventListener('click', function(){
-        if(pixel.style.backgroundColor === "white"){
-          pixel.style.backgroundColor="red"
-        }else{
-          pixel.style.backgroundColor="white"
-        }
-      })
+let squares = document.querySelectorAll('.square').forEach(function(pixel){
+  pixel.addEventListener('click', function(){
+    if(pixel.style.backgroundColor != color){
+      pixel.style.backgroundColor = color
+    }else{
+      pixel.style.backgroundColor = "white"
+    }
+  })
+})
 
-    let brushes = document.querySelector('.circle')
-    brushes.addEventListener("click",function (brush){
-      brush.forEach(console.log("color check!"))
-    })
-  }
+//color.toString ?!
+
+//   for (let i = 0; i < squares.length; i++) {
+//     let pixel = squares[i]
+//
+//     pixel.addEventListener('click', function(){
+//       if(pixel.style.backgroundColor === "white"){
+//         pixel.style.backgroundColor="red"
+//       }else{
+//         pixel.style.backgroundColor="white"
+//       }
+//     })
+// }
 // })
